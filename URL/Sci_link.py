30e184b093816c60f166a1ae8c4de82c9880c997
M.H.Li <99075651+HankLiTW@users.noreply.github.com>
2023-11-01 14:45:44 +0800

    upload file

--- URL/Sci_link.py
+++ URL/Sci_link.py
@@ -22,35 +22,39 @@ def extract_and_save_links_with_delay(input_csv_filename, output_csv_filename):
         next(csv_reader)  # Skip the header row
 
         writer = csv.writer(output_file)
-        writer.writerow(["Article Link"])  # Write the header row
+        writer.writerow(["URL"])  # Write the header row
 
+        current_index = 0
+        start_index = 275
         for row in csv_reader:
-            article_link = row[0]
-            headers = {'User-Agent': random.choice(user_agents)}  # Choose a random user agent
+            if current_index >= start_index:
+                article_link = row[0]
+                headers = {'User-Agent': random.choice(user_agents)}  # Choose a random user agent
 
-            response = requests.get(article_link, headers=headers)
+                response = requests.get(article_link, headers=headers)
 
-            if response.status_code == 200:
-                print(f"Successfully retrieved web content: {article_link}")
-                soup = BeautifulSoup(response.text, 'html.parser')
+                if response.status_code == 200:
+                    print(f"Successfully retrieved web content: {article_link}")
+                    soup = BeautifulSoup(response.text, 'html.parser')
 
-                # Extract article links
-                article_links = soup.select('a.article-content-title')
+                    # Extract article links
+                    article_links = soup.select('a.article-content-title')
 
-                for link in article_links:
-                    article_url = "https://www.sciencedirect.com" + link["href"]
-                    writer.writerow([article_url])
+                    for link in article_links:
+                        article_url = "https://www.sciencedirect.com" + link["href"]
+                        writer.writerow([article_url])
 
-                print(f"Successfully extracted links and wrote to {output_csv_filename}")
+                    print(f"Successfully extracted links and wrote to {output_csv_filename}")
 
-                # Add a random delay between requests (e.g., 2 to 5 seconds)
-                delay_seconds = random.uniform(1, 4)
-                time.sleep(delay_seconds)
-            else:
-                print(f"Failed to retrieve web content: {article_link}")
+                    # Add a random delay between requests (e.g., 2 to 5 seconds)
+                    delay_seconds = random.uniform(5, 10)
+                    time.sleep(delay_seconds)
+                else:
+                    print(f"Failed to retrieve web content: {article_link}")
+            current_index += 1
 
 if __name__ == "__main__":
-    journal_list = ["Journal of Economic Theory","Journal of Financial Economics",'Journal of International Economics','Journal of Monetary Economics','Journal of Urban Economics','Review of Economic Dynamics']
+    journal_list = ["Journal of Financial Economics"]
     with ThreadPoolExecutor(max_workers=len(journal_list)) as executor:
         for input_file in journal_list:
             # Set the output file name to "Journal of Development Economics.csv" for all input files
